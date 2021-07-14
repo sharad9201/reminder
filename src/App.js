ea1094662d7ae0c0fc31dedf18d3d4796f76e9b1
sharad9201 <sharad.rae19@gmail.com>
2021-07-14 23:47:15 +0545

    css setup

--- src/App.js
+++ src/App.js
@@ -1,12 +1,8 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useState } from 'react';
+import data from './data';
+import List from './List';
 function App() {
-  return (
-    <div className="App">
-    hey brother
-    </div>
-  );
+  return <h2>reminder project setup</h2>;
 }
 
 export default App;
