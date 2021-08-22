ade89a0878e8a6519469b2174f784f669346c875
sharad9201 <sharad.rae19@gmail.com>
2021-08-22 23:39:05 +0545

    random api

--- src/App.js
+++ src/App.js
@@ -1,17 +1,94 @@
-import React, { useState } from 'react';
-import data from './data';
-import List from './List';
-function App() {
-  const [reminder, setReminder] = useState(data)
+import React, { useState, useEffect } from 'react';
+
+// import data from './data';
+// import List from './List';
+
+
+const useFetch = (url) =>{
+  // const [questions, setQuestion] = useState(data);
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true)
+ 
+  useEffect(async ()=>{
+ 
+    const response = await fetch(url);
+    const data = await response.json(); 
+    const [item] = data.results;
+ 
+    setData(item);
+    setLoading(false);
+ 
+ },[]);
+ return {data, loading};
+ }
+
+ export default () =>{
+  // const [count, setCount] = useState(0);
+  const {data, loading} = useFetch("https://api.randomuser.me/")
+ 
   return <main>
-    <section className="container">
-      <h3 >{reminder.length} reminder games today </h3>
-      <List reminder={reminder}/>
-      <button onClick={()=> setReminder([])}>
-        clear all
-      </button>
-    </section>
+    <div className="">
+      {/* <p>You clicked {count} times</p> */}
+      {/* <button onClick={() =>setCount(count+1)}></button> */}
+      {loading ? <div>...loading</div>: <div>{data.name.first}</div>}
+    </div>
   </main>
 }
 
-export default App;
+// function App() {
+//   const [reminder, setReminder] = useState(data)
+//   return <main>
+//     <section className="container">
+//       <h3 >{reminder.length} reminder games today </h3>
+//       <List reminder={reminder}/>
+//       <button onClick={()=> setReminder([])}>
+//         clear all
+//       </button>
+//     </section>
+//   </main>
+// }
+
+// export default App;
+
+
+// import data from './data';
+// import SingleQuestion from './Question';
+
+// const useFetch = (url) =>{
+//  // const [questions, setQuestion] = useState(data);
+//  const [data, setData] = useState(null);
+//  const [loading, setLoading] = useState(true)
+
+//  useEffect(async ()=>{
+
+//    const response = await fetch(url);
+//    const data = await response.json(); 
+//    const [item] = data.results;
+
+//    setData(item);
+//    setLoading(false);
+
+// },[]);
+// return {data, loading};
+// }
+
+export default () =>{
+  const [count, setCount] = useState(0);
+  const {data, loading} = useFetch("https://api.randomuser.me/")
+ 
+  return <main>
+    <div className="">
+      <p>You clicked {count} times</p>
+      <button onClick={() =>setCount(count+1)}></button>
+      {loading ? <div>...loading</div>: <div>{data.name.first}</div>}
+      {/* <h3> Question and Answer</h3>
+      <section className="info">
+        { questions.map((question) => {
+            return <SingleQuestion key={question.id} {...question} />
+          })}
+      </section> */}
+    </div>
+  </main>
+}
+
+// export default App;
