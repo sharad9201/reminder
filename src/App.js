52f8aa8ef5b3978efec7fe886fc07b442ee79e8a
sharad9201 <sharad.rae19@gmail.com>
2021-08-23 09:51:46 +0545

    brought data form db

--- src/App.js
+++ src/App.js
@@ -1,54 +1,30 @@
 import React, { useState, useEffect } from 'react';
 
-// import data from './data';
-// import List from './List';
 
 
-const useFetch = (url) =>{
-  // const [questions, setQuestion] = useState(data);
-  const [data, setData] = useState(null);
-  const [loading, setLoading] = useState(true)
- 
-  useEffect(async ()=>{
- 
-    const response = await fetch(url);
-    const data = await response.json(); 
-    const [item] = data.results;
- 
-    setData(item);
-    setLoading(false);
- 
- },[]);
- return {data, loading};
- }
-
- export default () =>{
-  // const [count, setCount] = useState(0);
-  const {data, loading} = useFetch("https://api.randomuser.me/")
- 
+function App() {
+  const [reminders, setReminders] = useState([])
+  useEffect(()=>{
+    const url="http://127.0.0.1:8000/api/items";
+    fetch(url).then(resp=>resp.json())
+    .then(resp=>setReminders(resp))
+  },[])
   return <main>
-    <div className="">
-      {/* <p>You clicked {count} times</p> */}
-      {/* <button onClick={() =>setCount(count+1)}></button> */}
-      {loading ? <div>...loading</div>: <div>{data.name.first}</div>}
-    </div>
+    <section className="container">
+      {/* <h3 >{reminder.length} reminder games today </h3>
+      <List reminder={reminder}/>
+      <button onClick={()=> setReminder([])}>
+        clear all */}
+      {/* </button> */}
+
+      {
+        reminders.map(reminder=><div><p>{reminder.name}</p></div>)
+      }
+    </section>
   </main>
 }
 
-// function App() {
-//   const [reminder, setReminder] = useState(data)
-//   return <main>
-//     <section className="container">
-//       <h3 >{reminder.length} reminder games today </h3>
-//       <List reminder={reminder}/>
-//       <button onClick={()=> setReminder([])}>
-//         clear all
-//       </button>
-//     </section>
-//   </main>
-// }
-
-// export default App;
+export default App;
 
 
 // import data from './data';
