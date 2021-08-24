0f405e3b4ba7ebb46297595f273d54e102f454ea
sharad9201 <sharad.rae19@gmail.com>
2021-08-24 20:19:13 +0545

    does work but doesnot fetch

--- src/App.js
+++ src/App.js
@@ -3,82 +3,33 @@ import React, { useState, useEffect } from 'react';
 
 
 function App() {
-  const [reminders, setReminders] = useState([])
   
-  // const [reminder,setReminder]=useState("")
+    // const [todos, setTodos] = useState([])
+    const [name, setName] = useState("")
+    const handleSubmit = (e)=>{
+      e.preventDefault()
 
-  // function handleSubmit(e){
-  //   e.preventDefault();
+      const blog={name};
 
-  //   const newReminder = {
-  //     text,
-  //     completed: false,
-  //   }
-
-    // setReminders([...reminders].concat(newReminder))
-    // setReminder("")
-    // console.log(newReminder);
-
-    fetch ("http://127.0.0.1:8000/api/store",{
-      method: 'POST',
-      header: {"Content-Type":"application/json"},
-      body: JSON.stringify(newReminder)
-    }).then(()=>{
-      console.log('new data added')
-    })
-  }
-
-  useEffect(()=>{
-    getReminders();
-  },[])
-
-  function getReminders(){
-    const url="http://127.0.0.1:8000/api/items";
-    fetch(url).then(resp=>resp.json())
-    .then(resp=>setReminders(resp))
-  }
-  function reminderInsert(){
-    fetch("http://127.0.0.1:8000/api/store",{
-      method:'POST'
-    }).then((result)=>{
-      result.json().then((resp)=>{
-        console.warn(resp)
-        getReminders();
+      fetch ('http://127.0.0.1:8000/api/store',{
+        method: 'POST' ,
+        headers: {"Content-Type": "application/json"},
+        body: JSON.stringify(blog)
+      }).then(()=>
+      {
+        console.log('new blog added')
       })
-  })
-}
-  function reminderDelete(id){
-    // alert(id);
-    fetch(`http://127.0.0.1:8000/api/delete/${id}`,{
-      method:'DELETE'
-  }).then((result)=>{
-    result.json().then((resp)=>{
-      console.warn(resp)
-      getReminders();
-    })
-  })
-    
- 
-
+      console.log(blog)
+    }
   
   return <main>
     <section className="container">
-      
+      <form onSubmit={handleSubmit}> 
+        <input type="text" value={name} onChange={(e) =>setName(e.target.value)} name="text"/>
+        <button>Add Button</button>
+        <p>{name}</p>
+      </form>
 
-     {/* <div>
-       <form onSubmit={handleSubmit}>
-         <input type="text" onChange={(e)=>setReminder(e.target.value)} value={reminder.name} />
-          <button type="submit">Add Reminder</button>
-       </form>
-     </div> */}
-      {
-        reminders.map(reminder=><div><p>{reminder.name} </p> 
-
-          <button onClick={()=>reminderDelete(reminder.id)}>Delete</button>
-          
-         </div>)
-      }
-      <p>{reminder}</p>
 
       
     </section>
