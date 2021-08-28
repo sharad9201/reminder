43fe25de198da6d3656064fb08837a421b5d3b5b
sharad9201 <sharad.rae19@gmail.com>
2021-08-28 18:13:47 +0545

    todo added to db from here

--- src/App.js
+++ src/App.js
@@ -1,34 +1,46 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 
 
 
 function App() {
   
-    // const [todos, setTodos] = useState([])
-    const [name, setName] = useState("")
-    const handleSubmit = (e)=>{
-      e.preventDefault()
+    const [name, setName] = useState([])
+    // const [data, setdata] = useState("")
+    // const handleSubmit = (e)=>{
+    //   // e.preventDefault()
+    //   // const blog={name};
+    //   fetch ('http://127.0.0.1:8000/api/store',{
+    //     method: 'POST' ,
+    //     headers: {"Content-Type": "application/json"},
+    //     body: JSON.stringify(blog)
+    //   }).then(()=>
+    //   {
+    //     console.log('new blog added')
+    //   })
+    //   console.log(blog)
+    // }
 
-      const blog={name};
-
-      fetch ('http://127.0.0.1:8000/api/store',{
-        method: 'POST' ,
-        headers: {"Content-Type": "application/json"},
-        body: JSON.stringify(blog)
-      }).then(()=>
-      {
-        console.log('new blog added')
+    async function addTodo(){
+      const formData = new FormData()
+      formData.append("name",name)
+      let result = await fetch("http://127.0.0.1:8000/api/store",{
+        method:"POST",
+        body: formData,
+        
       })
-      console.log(blog)
+      alert ("todo was saved")
+
     }
   
   return <main>
     <section className="container">
-      <form onSubmit={handleSubmit}> 
-        <input type="text" value={name} onChange={(e) =>setName(e.target.value)} name="text"/>
-        <button>Add Button</button>
-        <p>{name}</p>
-      </form>
+      <h1>Here goes the todo</h1>
+      <div> 
+        <input type="text" className="form-control" onChange={(e) =>setName(e.target.value)} placeholder="enter the todo"/>
+        <button onClick={addTodo}>Add Button</button>
+        {/* <p>{name}</p> */}
+        
+      </div>
 
 
       
